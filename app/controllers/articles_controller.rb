acf3aa8de6432ab3ea8f84330cfc3394cbed5800
bryantt23 <bryantt23@gmail.com>
2015-03-16 21:59:44 -0700

    fin

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,31 @@
+class ArticlesController < ApplicationController
+
+  def index
+    @articles=Article.all
+  end
+
+  def show
+    @article=Article.find(params[:id])
+  end
+
+  def new
+    @article=Article.new
+  end
+
+  def create
+    @article=Article.new(articles_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def articles_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
